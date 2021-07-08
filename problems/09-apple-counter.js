66dc8763dabe6150a326cbd5cab02e21defcb610
Drewthurm21 <drewthurm21@gmail.com>
2021-07-07 19:19:01 -0500

    adding 9

--- problems/09-apple-counter.js
+++ problems/09-apple-counter.js
@@ -29,7 +29,11 @@ appleCounter(obj3); // => 3
 ***********************************************************************/
 
 function appleCounter(appleObj) {
-  // your code here
+  let count = 0;
+  for (key in appleObj) {
+    key.toLowerCase().includes("apple") ? ++count : null;
+  }
+  return count;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
