7573d0d0c0d882b0a2e7e69e77dbfed2fd566bb5
Drewthurm21 <drewthurm21@gmail.com>
2021-07-07 19:13:46 -0500

    updating 6 and 7

--- problems/06-key-in-object-array.js
+++ problems/06-key-in-object-array.js
@@ -24,7 +24,7 @@ function keyInObjectArray(objArray, keyString) {
   // declare a variable to hold true / false value
   let result = false;
   // loop through the array with for each
-  objArray.forEach(function (obj) {
+  objArray.forEach(obj => {
     // check if object contains the keyString as a key
     //if object contains the keyString change the variable to true
     if (keyString in obj) {
@@ -33,8 +33,20 @@ function keyInObjectArray(objArray, keyString) {
   })
 
   //return the result
-  return res
+  return result
 }
+
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" },
+  { name: "Drew", location: "undefined" }
+];
+
+console.log(keyInObjectArray(objArray, 'planet')); // => true
+console.log(keyInObjectArray(objArray, 'age'));// => true
+console.log(keyInObjectArray(objArray, 'food'));// => false
+console.log(keyInObjectArray(objArray, 'animal')); // => false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
